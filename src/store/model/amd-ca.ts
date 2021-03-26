6adea4346b4114b23d3df2a646eefee4a71426f3
Wael Elhomsi <waelhomsi94@gmail.com>
2021-03-27 00:17:03 +0200

    SS-36 Add AMD Direct and bandh

--- src/store/model/amd-ca.ts
+++ src/store/model/amd-ca.ts
@@ -43,6 +43,14 @@ export const AmdCa: Store = {
       series: 'rx6900xt',
       url: 'https://www.amd.com/en/direct-buy/5458372200/ca',
     },
+    {
+      brand: 'amd',
+      cartUrl:
+        'https://www.amd.com/en/direct-buy/5458373400/ca?add-to-cart=true',
+      model: 'amd reference',
+      series: 'rx6700xt',
+      url: 'https://www.amd.com/en/direct-buy/5458373400/ca',
+    },
   ],
   name: 'amd-ca',
 };
